24991267c622f25081b4b7f4caa8948e522c7bd7
Aryan Sawhney <aryansawhney3@gmail.com>
2020-09-30 18:14:28 +0530

    Completed

---
+++ app.js
@@ -0,0 +1,109 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const ejs = require("ejs");
+// let posts = [];
+var _ = require('lodash');
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const blogSchema =  new mongoose.Schema({
+  title: String,
+  body: String
+});
+
+const Blog = mongoose.model("Blog",blogSchema);
+
+
+app.get("/",function(req,res){
+  Blog.find({},function(err,blogList){
+    if(err){
+      console.log("Error");
+    }
+    else{
+      res.render("home",{
+        homeStartingRoute:homeStartingContent,
+        list:blogList
+      });
+    }
+  });
+
+});
+app.get("/about",function(req,res){
+  res.render("about",{
+    aboutContent:aboutContent
+  }); 
+});
+app.get("/contact",function(req,res){
+  res.render("contact",{
+    contactContent:contactContent
+  });
+});
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
+app.get("/posts/:testing",function(req,res){
+  const customPost = req.params.testing;
+  // posts.forEach(function(elements){
+  //   const customElement = elements.titleInput;
+  //   if(_.lowerCase(customPost) === _.lowerCase(customElement)){
+  //     res.render("post",{
+  //       postTitle:elements.titleInput,
+  //       postBody:elements.bodyInput
+  //     }); 
+  //   }
+  // });
+  Blog.findOne({title: customPost},function(err,obj){
+    if(err){
+      console.log("Error Found");
+    }
+    else{
+      console.log(obj);
+      if(_.lowerCase(customPost) === _.lowerCase(obj.title)){
+      res.render("post",{
+        postTitle:obj.title,
+        postBody:obj.body
+      });
+     }
+     else{
+       console.log("Did not match");
+     }
+
+    }
+
+  });
+});
+app.post("/compose",function(req,res){
+  // let post = {
+  //   titleInput:req.body.inputTitle,
+  //   bodyInput:req.body.inputBody
+  // }
+  // posts.push(post);
+  const blog = new Blog({
+    title: req.body.inputTitle,
+    body: req.body.inputBody
+  });
+  blog.save();
+  res.redirect("/");  
+  console.log("Saved");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
